e7d61769070a4df9460260261e953e8c09b83dd5
Assma1504 <assmabaziz15@gamil.com>
2026-03-17 01:12:43 +0300

    second option added to the third task

--- third_task.py
+++ third_task.py
@@ -1,10 +1,31 @@
+#=============== First Option ============================
+
 def check_magic_date():
     
     try:
-        year = int(input("Please enter the year: "))
+        year = int(input("Please enter the year: "))        
         month = int(input("Please enter the month: "))
         day = int(input("Please enter the day: "))
         lastTwoDigits = year % 100
+        productMontDay = month * day
+        if productMontDay == lastTwoDigits :
+            print("It's a magic year")
+        else: 
+            print("Ops,  it's not magic")
+    except ValueError:
+        print("Please enter a valid number")
+
+# check_magic_date()
+
+
+#=============== Second Option ============================
+def check_magic_date():
+    
+    try:
+        year = input("Please enter the year: ")
+        month = int(input("Please enter the month: "))
+        day = int(input("Please enter the day: "))
+        lastTwoDigits = int( year[-2:])
         productMontDay = month * day
         if productMontDay == lastTwoDigits :
             print("It's a magic year")
