a72dc493bbdd2ec33806232cf7042bb5145c6588
Assma1504 <assmabaziz15@gamil.com>
2026-03-17 00:45:44 +0300

    second option added to the first solution

--- first_task.py
+++ first_task.py
@@ -1,7 +1,18 @@
-def check_devision(num):
-    if num % 3 == 0:
-        print(f"{num} can be devided by 3")
-    else:
-        print(f"{num} cann't be devided by 3")
+#=============== First Option ============================
+# def check_devision(num):
+#     if num % 3 == 0:
+#         print(f"{num} can be devided by 3")
+#     else:
+#         print(f"{num} can't be devided by 3")
 
-check_devision(15)
+# check_devision(15)
+
+#=============== Second Option ============================
+def check_division_by_three():
+    userNumber =int(input("Please a number: "))
+    if userNumber % 3 == 0:
+        print(f"{userNumber} can be devided by 3")
+    else:
+        print(f"{userNumber} can't be devided by 3")
+
+check_division_by_three()   
